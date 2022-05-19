808b173d92a9ae428a244c6664e8eedf12ba1e73
bti <jeremybettini13@gmail.com>
2022-05-19 22:19:04 +0200

    er

--- CatcheurPickerApp/app/src/main/java/fr/maxime/catcheurpicker/MainActivity.java
+++ CatcheurPickerApp/app/src/main/java/fr/maxime/catcheurpicker/MainActivity.java
@@ -5,7 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.maxime.catcheurpicker.BD.CatcheurViewModel;
+import fr.maxime.catcheurpicker.BD.TeamViewModel;
+import fr.maxime.catcheurpicker.Model.Catcheur;
+import fr.maxime.catcheurpicker.Model.Team;
+
 public class MainActivity extends AppCompatActivity {
+    private List<Catcheur> dataCatcheur = new ArrayList<>();
+    private List<Team> dataTeam = new ArrayList<>();
+    private CatcheurViewModel catcheurViewModel;
+    private TeamViewModel teamViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
