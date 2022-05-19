74a2c1862bac627fc38ffb86c74bf1db5df089f4
Maxime Sevot <maxime.sevot@etu.univ-orleans.fr>
2022-05-19 15:51:20 +0200

    er

--- CatcheurPickerApp/app/src/main/java/fr/maxime/catcheurpicker/MainActivity.java
+++ CatcheurPickerApp/app/src/main/java/fr/maxime/catcheurpicker/MainActivity.java
@@ -3,12 +3,15 @@ package fr.maxime.catcheurpicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_catcheurs);
     }
 }
