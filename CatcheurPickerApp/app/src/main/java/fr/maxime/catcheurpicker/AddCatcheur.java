ce96356bd5b7c5214a63e70ffda0acb514521a66
Maxime Sevot <maxime.sevot@etu.univ-orleans.fr>
2022-05-20 13:33:32 +0200

    Fusion fonctionnel

--- CatcheurPickerApp/app/src/main/java/fr/maxime/catcheurpicker/AddCatcheur.java
+++ CatcheurPickerApp/app/src/main/java/fr/maxime/catcheurpicker/AddCatcheur.java
@@ -1,13 +1,36 @@
 package fr.maxime.catcheurpicker;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.maxime.catcheurpicker.BD.CatcheurViewModel;
+import fr.maxime.catcheurpicker.BD.TeamViewModel;
+import fr.maxime.catcheurpicker.Model.Catcheur;
+import fr.maxime.catcheurpicker.Model.Team;
+import fr.maxime.catcheurpicker.Tools.CustomAdapterCatcheur;
+import fr.maxime.catcheurpicker.Tools.CustomAdapterTeam;
+import fr.maxime.catcheurpicker.Tools.InterfaceGestionClick;
 
 public class AddCatcheur extends AppCompatActivity {
+
+    private List<Catcheur> dataCatcheur = new ArrayList<>();
+    private List<Team> dataTeam = new ArrayList<>();
+    private LinearLayoutManager linearLayoutManager;
+    private CustomAdapterCatcheur customAdapterCatcheur;
+    private CustomAdapterTeam customAdapterTeam;
+    private CatcheurViewModel catcheurViewModel;
+    private TeamViewModel teamViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +38,54 @@ public class AddCatcheur extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_add_catcheur);
+        catcheurViewModel = new ViewModelProvider(this).get(CatcheurViewModel.class);
+        teamViewModel = new ViewModelProvider(this).get(TeamViewModel.class);
+        //RecyclerView recyclerView = findViewById(R.id.recyclerview); pour la liste de catcheur ou team
+        linearLayoutManager = new LinearLayoutManager(this);
+        customAdapterCatcheur = new CustomAdapterCatcheur(dataCatcheur);
+        customAdapterTeam = new CustomAdapterTeam(dataTeam);
+
+
+        CustomAdapterCatcheur.setMyGestionClick(new InterfaceGestionClick() {
+        @Override
+        public void onItemClick(int position, View v) {
+            Log.d("MesLogs","onItemClick MainActivity");
+            Catcheur catcheur = dataCatcheur.get(position);
+            new AlertDialog.Builder(v.getContext())
+                    .setTitle(catcheur.getNomScene())
+                    .setMessage("Poids: "+catcheur.getTaille() + " Taille : "+catcheur.getTaille())
+                    .show();
+        }
+
+        @Override
+        public void onItemLongClick(int position, View view) {
+            catcheurViewModel.deleteOneCatcheur(dataCatcheur.get(position));
+        }
+    });
+
+        CustomAdapterTeam.setMyGestionClick(new InterfaceGestionClick() {
+        @Override
+        public void onItemClick(int position, View v) {
+            Log.d("MesLogs","onItemClick ShowCatcheurs");
+            Team team = dataTeam.get(position);
+            new AlertDialog.Builder(v.getContext())
+                    .setTitle(team.getNomTeam())
+                    .setMessage("Nom de la team: "+team.getNomTeam())
+                    .show();
+        }
+
+        @Override
+        public void onItemLongClick(int position, View view) {
+            teamViewModel.deleteOneTeam(dataTeam.get(position));
+        }
+    });
+}
+
+    public void addCatcheur(View view) {
+        System.out.println("ICICICICICIICICICICI " + R.id.fieldNomCatcheur);
+        catcheurViewModel.insert(new Catcheur(R.id.fieldNomCatcheur+"", R.id.fieldPoids, R.id.fieldTaille, "image", R.id.fieldNomCatcheur+""));
     }
+
 
     public void goToAddTeam(View view){
         Intent intent = new Intent(this, AddTeam.class);
--- CatcheurPickerApp/app/src/main/java/fr/maxime/catcheurpicker/ShowCatcheurs.java
+++ CatcheurPickerApp/app/src/main/java/fr/maxime/catcheurpicker/ShowCatcheurs.java
@@ -1,13 +1,36 @@
 package fr.maxime.catcheurpicker;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.maxime.catcheurpicker.BD.CatcheurViewModel;
+import fr.maxime.catcheurpicker.BD.TeamViewModel;
+import fr.maxime.catcheurpicker.Model.Catcheur;
+import fr.maxime.catcheurpicker.Model.Team;
+import fr.maxime.catcheurpicker.Tools.CustomAdapterCatcheur;
+import fr.maxime.catcheurpicker.Tools.CustomAdapterTeam;
+import fr.maxime.catcheurpicker.Tools.InterfaceGestionClick;
 
 public class ShowCatcheurs extends AppCompatActivity {
+    private List<Catcheur> dataCatcheur = new ArrayList<>();
+    private List<Team> dataTeam = new ArrayList<>();
+    private LinearLayoutManager linearLayoutManager;
+    private CustomAdapterCatcheur customAdapterCatcheur;
+    private CustomAdapterTeam customAdapterTeam;
+    private CatcheurViewModel catcheurViewModel;
+    private TeamViewModel teamViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +38,50 @@ public class ShowCatcheurs extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_show_catcheurs);
+        catcheurViewModel = new ViewModelProvider(this).get(CatcheurViewModel.class);
+        teamViewModel = new ViewModelProvider(this).get(TeamViewModel.class);
+        //RecyclerView recyclerView = findViewById(R.id.recyclerview); pour la liste de catcheur ou team
+        linearLayoutManager = new LinearLayoutManager(this);
+        customAdapterCatcheur = new CustomAdapterCatcheur(dataCatcheur);
+        customAdapterTeam = new CustomAdapterTeam(dataTeam);
+
+        CustomAdapterCatcheur.setMyGestionClick(new InterfaceGestionClick() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Log.d("MesLogs","onItemClick MainActivity");
+                Catcheur catcheur = dataCatcheur.get(position);
+                new AlertDialog.Builder(v.getContext())
+                        .setTitle(catcheur.getNomScene())
+                        .setMessage("Poids: "+catcheur.getTaille() + " Taille : "+catcheur.getTaille())
+                        .show();
+            }
+
+            @Override
+            public void onItemLongClick(int position, View view) {
+                catcheurViewModel.deleteOneCatcheur(dataCatcheur.get(position));
+            }
+        });
+
+        CustomAdapterTeam.setMyGestionClick(new InterfaceGestionClick() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Log.d("MesLogs","onItemClick MainActivity");
+                Team team = dataTeam.get(position);
+                new AlertDialog.Builder(v.getContext())
+                        .setTitle(team.getNomTeam())
+                        .setMessage("Nom de la team: "+team.getNomTeam())
+                        .show();
+            }
+
+            @Override
+            public void onItemLongClick(int position, View view) {
+                teamViewModel.deleteOneTeam(dataTeam.get(position));
+            }
+        });
+    }
+
+    public void addCatcheur(View view) {
+        catcheurViewModel.insert(new Catcheur(R.id.fieldNomCatcheur+"", R.id.fieldPoids, R.id.fieldTaille, "image", R.id.fieldNomCatcheur+""));
     }
 
     public void goToAddCatcheur(View view){
@@ -40,4 +107,5 @@ public class ShowCatcheurs extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
 }
